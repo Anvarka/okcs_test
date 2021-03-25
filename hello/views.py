b7d2a26c009494d89860f9f7dc546c438da4e893
grazder <grazder@yandex.ru>
2021-03-25 09:40:04 +0300

    link

--- hello/views.py
+++ hello/views.py
@@ -8,17 +8,21 @@ def webhook(request):
     request_message = json.loads(request.body)
 
     text = ''
+    link = 'https://vk.cc/c06npA'
+    voice = ''
 
     if request_message['session']['new']:
-        text = 'Привет новичек'
+        voice = 'Привет, чтобы читать ленту, тебе нужно сначала залогиниться в ОК. '
+        text = voice + link
     else:
         text = request_message['request']['original_utterance']
+        voice = text
 
     derived_session_fields = ['session_id', 'user_id', 'message_id']
     response_message = {
         "response": {
             "text": text,
-            "tts": text,
+            "tts": voice,
             "end_session": False
         },
         "session": {derived_key: request_message['session'][derived_key] for derived_key in derived_session_fields},
@@ -26,3 +30,6 @@ def webhook(request):
     }
 
     return JsonResponse(response_message, safe=False)
+
+def button(title):
+    return {"title": title}
